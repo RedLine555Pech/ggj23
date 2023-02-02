d6ac7faa6e964a7614a15877ba4db2e4321e399a
nykyta.yevdokymenko@chatbots.studio+1-at-261222764291 <nykyta.yevdokymenko@chatbots.studio>
2023-02-02 12:31:08 +0200

    get_node_or_null

--- src/characters/BaseCharacter.gd
+++ src/characters/BaseCharacter.gd
@@ -6,18 +6,18 @@ var is_dragging = false;
 export var type = "EGG"
 
 func _ready() -> void:
-	var dragable = get_node("Dragable")
+	var dragable = get_node_or_null("Dragable")
 	if dragable:
 		dragable.connect("on_drag_started", self, "on_drag_started");
 		dragable.connect("on_drag_finished", self, "on_drag_finished");
-	var hoverable = get_node("Hoverable")
+	var hoverable = get_node_or_null("Hoverable")
 	if hoverable:
 		hoverable.connect("on_hover_started", self, "on_hover_started");
 		hoverable.connect("on_hover_finished", self, "on_hover_finished");
-	var interactable = get_node("Interactable")
+	var interactable = get_node_or_null("Interactable")
 	if interactable:
 		interactable.connect("on_interaction", self, "on_interaction");
-	var clickable = get_node("Clickable")
+	var clickable = get_node_or_null("Clickable")
 	if clickable:
 		clickable.connect("on_clicked", self, "on_clicked");
 		
