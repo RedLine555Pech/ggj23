5aefd01482af01e249ae078e433def33c080a66e
nykyta.yevdokymenko@chatbots.studio+1-at-261222764291 <nykyta.yevdokymenko@chatbots.studio>
2023-02-03 14:12:22 +0200

    added click and drag

--- src/characters/PropertyNodes/Clickable/Clickable.gd
+++ src/characters/PropertyNodes/Clickable/Clickable.gd
@@ -2,6 +2,16 @@ extends Area2D
 
 signal on_clicked
 
+var mouse_moved = false;
+var left_clicked = false;
+
 func _on_Clickable_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton && event.is_action("left_click"):
-		emit_signal("on_clicked")
+	if event is InputEventMouseButton && event.is_action_pressed("left_click"):
+		left_clicked = true;
+		mouse_moved = false;
+	elif left_clicked && !mouse_moved && event is InputEventMouseButton && event.is_action_released("left_click"):
+		left_clicked = false;
+		emit_signal("on_clicked")	
+	elif left_clicked && event is InputEventMouseMotion:
+		mouse_moved = true;
+		
