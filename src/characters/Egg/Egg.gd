e3d7cac85beebd964c99e75c6aad8ba3d0efd22f
Bohdan Matviiv <bomtvv@gmail.com>
2023-02-03 19:34:38 +0200

    start logic

--- src/characters/Egg/Egg.gd
+++ src/characters/Egg/Egg.gd
@@ -1,5 +1,10 @@
 extends BaseCharacter
 
+export var on_tree: bool = false
+export var breakable: bool = false
+export var health: int = 3
+export var spawn: PackedScene
+
 func on_drag_started() -> void:
 	$Sprite.modulate = Color(0,1,0);
 	.on_drag_started()
@@ -13,3 +18,45 @@ func on_drag_finished() -> void:
 func on_interaction(obj: BaseCharacter) -> void:
 	if obj.type == "CHICKEN" && is_dragging:
 		queue_free();
+
+func on_clicked():
+	if on_tree: return
+	
+	.on_clicked()
+	damage()
+
+
+func fall():
+	yield(get_tree().create_timer(1), "timeout")
+	on_tree = false
+	
+	if breakable:
+		kill()
+	else:
+		damage()
+
+
+func damage():
+	health = clamp(health - 1, 0, health)
+	print("Damaged" + str(health))
+	
+	if health <= 0:
+		kill()
+
+
+func kill():
+	print("Killed")
+	
+	if spawn:
+		var obj: Node2D = spawn.instance()
+		obj.scale = Vector2.ZERO
+		obj.position = position
+		get_parent().add_child(obj)
+		
+		var tween = get_tree().create_tween()
+		tween.tween_property(obj, "scale", Vector2(1, 1), 0.5)
+		
+		yield(tween, "finished")
+	
+	queue_free()
+	
---
+++ src/characters/Tree/Tree.gd
@@ -0,0 +1,28 @@
+extends BaseCharacter
+
+export(Array, NodePath) var path_eggs
+export(float, 0.1, 1) var click_delay := 0.3
+
+var eggs: Array = []
+var clicked: int = 0
+var can_click: bool = true
+
+func _ready() -> void:
+	for path in path_eggs:
+		eggs.append(get_node(path))
+
+func on_clicked():
+	.on_clicked()
+	if !can_click: return
+	
+	can_click = false
+	
+	if clicked < eggs.size() && eggs[clicked].has_method("fall"):
+		eggs[clicked].fall()
+	
+	clicked += 1
+	$DelayTimer.start(click_delay)
+
+
+func _on_DelayTimer_timeout() -> void:
+	can_click = true
