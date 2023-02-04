be895c85c839b7d0dfc77c5f2755de8d8c1c2815
Bohdan Matviiv <bomtvv@gmail.com>
2023-02-04 19:25:31 +0200

    lock achievement design

--- gd-achievements/Achievement/Achievement.gd
+++ gd-achievements/Achievement/Achievement.gd
@@ -4,6 +4,8 @@ class_name AchievementItem
 export(NodePath) onready var title_box = get_node(title_box) as Label
 export(NodePath) onready var description_box = get_node(description_box) as Label
 export(NodePath) onready var texture_rect = get_node(texture_rect) as TextureRect
+export(Color) var lock_modulate
+export(Color) var unlock_modulate
 
 var key: String = Achievements.NAME.INSTANT
 var unlocked: bool = true
@@ -24,6 +26,8 @@ func set_key(new_key: String):
 
 func set_unlocked(value: bool):
 	unlocked = value
+	
+	modulate = unlock_modulate if value else lock_modulate
 
 
 func achievement_unlocked(achievement, achievement_key):
